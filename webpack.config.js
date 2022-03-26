c67b4bb64b510cff709e6bbb8c2cc54e0ec6d566
skynono <feitianbubu@qq.com>
2022-03-27 01:36:46 +0800

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{loader:'style-loader'}, {loader:'css-loader'}]
+        }, {
+            test: /\.scss$/,
+            use: [{loader:'style-loader'}, {loader:'css-loader'}, {loader:'sass-loader'}]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: 'img/[name].[hash:7].[ext]'
+            }
+        }, {
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    devServer: {
+        // contentBase: './build',
+        port: 8081, // 端口号
+        // inline: true,
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlPlugin({
+            template: 'public/index.html'
+        })
+    ]
+}
