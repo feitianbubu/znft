802ac05558340a63871e0cdd6d2c701ca52958a2
skynono <feitianbubu@qq.com>
2022-03-29 21:48:51 +0800

    feat: opt img

--- src/Header.js
+++ src/Header.js
@@ -5,19 +5,17 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {FormControl} from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 import _ from 'lodash';
-
-import Paper from '@mui/material/Paper';
 
 let web3;
 let user = {};
 let connectBtnName = '连接钱包';
+let contractBtnName = '获取信息';
+let contractBtnNameDisabled = false;
 
 function UserInfo(props) {
     console.log('UserInfo: ', props);
@@ -34,7 +32,7 @@ function UserInfo(props) {
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: '', rows: [], contractAddress:''};
+        this.state = {value: '', rows: [], contractAddress:'', itemData: [], user: {}, snackbarOpen: false, snackbarMsg: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -66,6 +64,9 @@ class App extends React.Component {
     };
     handleSubmit = async () => {
         let self = this;
+        contractBtnName = '正在获取...';
+        contractBtnNameDisabled = true;
+        self.forceUpdate();
         console.log('handleSubmit', user, this.state);
         if (!user.account) {
             this.setState({snackbarMsg: "请先连接钱包"});
@@ -76,18 +77,25 @@ class App extends React.Component {
         let myContract = new web3.eth.Contract(jsonInterface, this.state.contractAddress);
 
 
-        let rows = [];
-        let value =await myContract.methods.balanceOf(user.account).call();
-        let name = 'balanceOf';
-        let row = {name, value};
-        rows.push(row);
-        self.setState({rows});
+        let balance =await myContract.methods.balanceOf(user.account).call();
+        self.setState({contactBalance: `balance: ${balance}`});
 
-        // _.times(value, async function (i) {
-        // });
-
-        let index = await myContract.methods.tokenByIndex('0').call();
-        console.log('tokenByIndex', index);
+        let itemData = [];
+        for await( const i of  _.range(balance)) {
+            let index = await myContract.methods.tokenByIndex(i).call();
+            console.log('tokenByIndex', index);
+            let tokenURI = await myContract.methods.tokenURI(index).call();
+            console.log('tokenURI', tokenURI);
+            if(!_.startsWith(tokenURI, 'http')) {
+                continue;
+            }
+            let row = {title: index, img: tokenURI};
+            itemData.push(row);
+            self.setState({itemData});
+            self.forceUpdate();
+        }
+        contractBtnName = '获取信息'
+        contractBtnNameDisabled = false;
         self.forceUpdate();
     };
 
@@ -116,35 +124,32 @@ class App extends React.Component {
                                 <div><TextField id="contractAddress" label="合约地址" value={this.state.contractAddress} required
                                                 sx={{m: 1, width: '25ch'}}
                                                 onChange={this.handleChange}/></div>
-                                <div><Button type="button" variant="contained"
-                                             onClick={this.handleSubmit}>获取合约信息</Button></div>
+                                <div><Button type="button" variant="contained" disabled={contractBtnNameDisabled}
+                                             onClick={this.handleSubmit}>{contractBtnName}</Button></div>
                             </FormControl>
                         </form>
                     </div>
 
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>name</TableCell>
-                                    <TableCell align="right">value</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {this.state.rows.map((row) => (
-                                    <TableRow
-                                        key={row.name}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell component="th" scope="row">
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell align="right">{row.value}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                    <div>{this.state.contactBalance}</div>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                        {this.state.itemData.map((item) => (
+                            <ImageListItem key={item.title}>
+                                <img
+                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={item.title}
+                                    loading="lazy"
+                                />
+                                <Button variant="contained" endIcon={<CardGiftcardIcon />}>
+                                    赠送
+                                </Button>
+                                <ImageListItemBar
+                                    title={`tokenId: ${item.title}`}
+                                    position="below"
+                                />
+                            </ImageListItem>
+                        ))}
+                    </ImageList>
 
                     <div>
                         <Snackbar
