d206f3003ff8cd3ce6c4fd209d0df06e733b3bfa
skynono <feitianbubu@qq.com>
2022-06-06 16:53:33 +0800

    feat: set dark mode

--- packages/pc/src/components/home/nav/index.tsx
+++ packages/pc/src/components/home/nav/index.tsx
@@ -1,28 +1,34 @@
 import React, {useCallback} from "react";
-import {AppBar, Box, Button, Divider} from '@mui/material'
+import {AppBar, Box, Button, Chip, Divider} from '@mui/material'
 import {styled} from '@mui/material/styles';
 import {useWallet} from "@/pc/context/wallet";
 import {message} from "@lib/util";
+import PersonIcon from '@mui/icons-material/Person';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import TuneIcon from '@mui/icons-material/Tune';
 
+const pages = [
+    {name: '市场', id: 'market'},
+    {name: '我的', id: 'my'},
+    {name: '空投', id: 'mint'},
+    {name: '兑换', id: 'swap'}
+];
 const Banner = styled("div")({
     padding: '5px 5px',
     display: 'flex',
     justifyContent: "space-between"
 })
-const UserBox = styled('div')({
-    height: 20,
-})
+const UserBox = styled('div')({display: 'flex', alignItems: 'center', marginRight: '20px'})
 const ActionBox = styled('div')({display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end'})
-const Name = styled('span')({
-    color: '#bdbdbd',
-    paddingLeft: '8px'
-})
 const Logo = styled('div')({
+    margin: '0 20px',
     fontWeight: 'bold',
     fontSize: 32
 })
 const Nav: React.FC = () => {
     const {address, balance, chainId} = useWallet();
+    let showAddress = address && address.substring(0, 6) + '...' + address.substring(address.length - 4);
+    let showBalance = parseFloat(balance).toFixed(4)
     let connectBtnName = '连接钱包';
     const url = process.env.NEXT_PUBLIC_API_URL;
     const handleClick = useCallback(async () => {
@@ -49,7 +55,11 @@ const Nav: React.FC = () => {
         } catch (e) {
             message.warning(`清除失败:${e}`);
         }
-    }, [address, chainId, url])
+    }, [address, chainId, url]);
+    const [selectPageId, setSelectPageId] = React.useState<string>(pages[0].id);
+    const handleChangePage = useCallback((id: string) => {
+        setSelectPageId(id);
+    }, [setSelectPageId]);
 
     return <>
         <AppBar position="relative">
@@ -57,17 +67,27 @@ const Nav: React.FC = () => {
                 <Logo>
                     Z-NFT
                 </Logo>
+                <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                    {pages.map((page) => (<Button
+                        color={selectPageId === page.id ? 'warning' : 'inherit'}
+                        key={page.id}
+                        id={page.id}
+                        size={'large'}
+                        sx={{
+                            fontWeight: 'bold', fontSize: '1.2rem', mr: 2, display: {xs: 'none', md: 'flex'}
+                        }}
+                        onClick={() => handleChangePage(page.id)}
+                    >
+                        {page.name}
+                    </Button>))}
+                </Box>
                 <Box>
                     <UserBox>
-                        <span>
-                            <Name>account:</Name>{address}
-                        </span>
-                        <span>
-                            <Name>balance:</Name>{parseFloat(balance).toFixed(4)}
-                        </span>
-                        <span>
-                            <Name>chainId:</Name>{chainId}
-                        </span>
+                        <Chip sx={{border: '0'}} icon={<PersonIcon fontSize="small"/>} label={showAddress}
+                              variant="outlined" clickable={true}/>
+                        <Chip sx={{border: '0'}} icon={<AttachMoneyIcon fontSize="small"/>} label={showBalance} variant="outlined"
+                              clickable={true}/>
+                        <Chip sx={{border: '0'}} icon={<TuneIcon fontSize="small"/>} label={chainId} variant="outlined" clickable={true}/>
                     </UserBox>
                     <ActionBox>
                         <Button color="inherit" onClick={handleClearCache}>刷新缓存</Button>
