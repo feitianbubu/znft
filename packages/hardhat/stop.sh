f238552caabdd71b9317d16af59c3f539493c093
skynono <feitianbubu@qq.com>
2022-06-20 13:51:23 +0800

    feat: opt hardhat stop.sh

--- packages/hardhat/stop.sh
+++ packages/hardhat/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kill -2 $(ps aux | grep namespace=chainServer | grep ./chain | grep -v grep | awk '{print $2}') 2>/dev/null
+kill -2 $(ps aux | grep 'hardhat node' | grep -v grep | awk '{print $2}') 2>/dev/null
