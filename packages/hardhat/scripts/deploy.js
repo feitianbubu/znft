679279d983c588a9aa1377bebed679ad8b42aa93
skynono <feitianbubu@qq.com>
2022-07-13 18:20:56 +0800

    docs: add opensea1155

--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -8,7 +8,7 @@ task("check-balance", "Prints out the balance of your account").setAction(async 
 });
 
 task("deploy", "Deploys the NFT.sol contract").setAction(async function (taskArguments, hre) {
-    const nftContractFactory = await hre.ethers.getContractFactory("ERC1155Tradable", getAccount());
-    const nft = await nftContractFactory.deploy("znft","ZNFT","0xf57b2c51ded3a29e6891aba85459d600256cf317");
+    const nftContractFactory = await hre.ethers.getContractFactory("MyFactory", getAccount());
+    const nft = await nftContractFactory.deploy("0xf57b2c51ded3a29e6891aba85459d600256cf317","0x3405eCD655c5EC805AaFf17b4b58210d1074a052");
     console.log(`Contract deployed to address: ${nft.address}`, taskArguments);
 });
